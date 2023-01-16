ea122753b147e3b7368f58783f3054eceb5c990a
guchigow <lanov64@yandex.kz>
2023-01-16 13:23:03 +0600

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openArr = [];
+  let closeArr = [];
+  let resArr = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openArr.push(bracketsConfig[i][0]);
+    closeArr.push(bracketsConfig[i][1]);
+  }
+  for (let i = 0; i < str.length; i += 1) {
+    const sSym = str[i];
+    let last = resArr.length - 1;
+    let openItemIndex = closeArr.indexOf(sSym);
+    if (openArr.includes(sSym)) {
+      if (closeArr.includes(sSym)) {
+        if (resArr.indexOf(sSym) < 0) {
+          resArr.push(sSym);
+        } else {
+          if (resArr[last] === sSym) {
+            resArr.pop();
+          } else return false;
+        }
+      }
+      else resArr.push(sSym);
+    }
+    else if (closeArr.includes(sSym)) {
+      if (resArr[last] === openArr[openItemIndex]) {
+        resArr.pop();
+        continue;
+      } else return false;
+    }
+  }
+  return resArr.length === 0;
 }
